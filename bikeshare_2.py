188f35ebdfc17bcd33a97d2039e8f525ad70a2aa
Parag Sasturkar <sasturkarparag@gmail.com>
2020-06-06 16:14:12 -0700

    added a title to the project

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,19 +16,19 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    
+
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    
+
     print('\nWhich city you want to explore: Chicago, New York, or  Washington? Enter only one')
-    
+
     city = ''
-    
+
     while city not in ('Chicago', 'New York', 'Washington'):
-        
+
         city_name = input().title()
-        
+
         print('\nCity Selected: {}'.format(city_name))
-        
+
         if city_name not in ('Chicago', 'New York', 'Washington'):
             print('\nWrong Selection! Please enter only 1 value from these 3 options: Chicago, New York, or  Washington')
             continue
@@ -38,15 +38,15 @@ def get_filters():
     # TO DO: get user input for month (all, january, february, ... , june)
 
     print('\nEnter a month to explore: all, january, february, march, april, may, or june? Enter only one')
-    
+
     month = ''
-    
+
     while month not in ('All', 'January', 'February', 'March', 'April', 'May', 'June'):
-        
-        month_name = input().title() 
-        
+
+        month_name = input().title()
+
         print('\nMonth Selected: {}'.format(month_name))
-        
+
         if month_name not in ('All', 'January', 'February', 'March', 'April', 'May', 'June'):
             print('\nWrong Selection! Please enter only 1 value from these 7 options: all, january, february, march, april, may, or june')
             continue
@@ -58,13 +58,13 @@ def get_filters():
     print('\Enter a day of week to explore: all, monday, tuesday, wednesday, thursday, friday, saturday, or sunday? Enter only one')
 
     day = ''
-    
+
     while day not in ('All', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'):
-        
-        day_name = input().title() 
-        
+
+        day_name = input().title()
+
         print('\nDay Selected: {}'.format(day_name))
-        
+
         if day_name not in ('All', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'):
             print('\nWrong Selection! Please enter only 1 value from these 8 options: all, monday, tuesday, wednesday, thursday, friday, saturday, or sunday')
             continue
@@ -73,7 +73,7 @@ def get_filters():
 
     print('-'*40)
     print('-'*40)
-    
+
     return city, month, day
 
 
@@ -89,18 +89,18 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
     print('\nPlease wait, the data is loading ...\n')
-    
+
     df = pd.read_csv(CITY_DATA[city])
-    
+
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-    
+
     if month != 'All':
         months = ['January', 'February', 'March', 'April', 'May', 'June']
         month = months.index(month)+1
         df = df[df['month']==month]
-    
+
     if day != 'All':
         df = df[df['day_of_week']==day]
 
@@ -142,9 +142,9 @@ def station_stats(df):
     # TO DO: display most frequent combination of start station and end station trip
     start = df.groupby(['Start Station','End Station']).size().nlargest(1).reset_index()[['Start Station','End Station']].iloc[0][0]
     end = df.groupby(['Start Station','End Station']).size().nlargest(1).reset_index()[['Start Station','End Station']].iloc[0][1]
-    
+
     print('\nThe most frequent combination of start station and end station trip is:\n{}\nTO\n{}'.format(start,end))
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -170,23 +170,23 @@ def user_stats(df):
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
-    
+
     # TO DO: Display counts of user types
     print('\nThere are total {} user types.'.format(df['User Type'].value_counts().count()))
 
     if 'Gender' in list(df.columns):
         # TO DO: Display counts of gender
         print('\nThere are total {} genders.'.format(df['Gender'].value_counts().count()))
-    
+
     else:
         print('\nGender data not available for this city.')
-    
+
     if 'Birth Year' in list(df.columns):
         # TO DO: Display earliest, most recent, and most common year of birth
         print('\nEarliest birth year: {}'.format(df['Birth Year'].min()))
         print('\nMost Recent birth year: {}'.format(df['Birth Year'].max()))
         print('\nMost Common birth year: {}'.format(df['Birth Year'].mode()[0]))
-    
+
     else:
         print('\nBirth Year data not available for this city.')
 
@@ -204,6 +204,7 @@ def display_data(df):
 
 def main():
     while True:
+        print("Welcome to the US Bikeshare World")
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
@@ -212,7 +213,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         display_data(df)
-        
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
